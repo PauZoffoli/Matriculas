063bf73bab850dbbca45bdb38751032a28233bb2
Zerokorg <zerokorgpozo@gmail.com>
2018-09-27 05:54:17 -0300

    botones save a guardar

--- resources/views/MatriculaPostulante/alumnos/edit.blade.php
+++ resources/views/MatriculaPostulante/alumnos/edit.blade.php
@@ -77,7 +77,7 @@
         </h1> <br>
 </section>
         <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
        <input type="button" name="clear" value="CLEAR ALL DATOS PRUEBA" onclick="clearForm(this.form);" >
 
 </div>
@@ -248,7 +248,7 @@ $primerContacto
                         
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
     
 </div>
 
